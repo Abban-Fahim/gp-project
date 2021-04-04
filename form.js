292e19ccbe35c2002028d705818d7b3b1aac8baf
AbbanFahim <lloydtechno58@gmail.com>
2021-04-04 20:42:14 +0400

    finished form

--- form.js
+++ form.js
@@ -1,9 +1,38 @@
-exports.radioData = [{
+exports.radioData = [
+    {
+        title: 'Please enter your name',
+        name: 'name',
+        type: 'text'
+    },
+    {
+        title: 'Please specify your gender?',
+        name: 'gender',
+        type: 'radio',
+        options: ['Male', 'Female', 'Other']
+    },
+    {
+        title: 'What are your hobbies?',
+        name: 'hobbies',
+        type: 'text'
+    },
+    {
         title: 'How often do you practise your hobbies?',
         name: 'howoften',
         type: 'radio',
         options: ['Once a month', 'Once every week', 'A few times a week', 'Everyday']
-},
+    },
+    {
+        title: 'Are you satisfied with your physical health?',
+        name: 'physicalH',
+        type: 'radio',
+        options: ['Yes', 'No', 'A bit']
+    },
+    {
+        title: 'Are you satisfied with your mental health?',
+        name: 'mentalH',
+        type: 'radio',
+        options: ['Yes', 'No', 'A bit']
+    },
     {
         title: 'How often do you excercise?',
         name: 'amontOfEx',
@@ -14,5 +43,27 @@ exports.radioData = [{
         title: 'What motivates/keeps you from physical activity?',
         name: 'motivation',
         type: 'text'
+    },
+    {
+        title: 'Do you consume a balanced diet?',
+        name: 'boolean-balanced',
+        type: 'text'
+    },
+    {
+        title: 'If you do practise a physical activity, how long do you do it for in one day?',
+        name: 'howLong?',
+        type: 'radio',
+        options: ['< 30 minutes', '< 1 hour', '< 2 hours', '> 2 hours']
+    },
+    {
+        title: 'How do you think irregular/ no exercise might affect ones health?',
+        name: 'Uaware?',
+        type: 'text'
+    },
+    {
+        title: 'Do you enjoy practising physical activities?',
+        name: 'doULike',
+        type: 'radio',
+        options: ['Yes', 'No', 'Occasionaly']
     }
-]
+];
