23d53a73d692e07bcc8423afc2ef04566ce4e81e
Abban-Fahim <lloydtechno58@gmail.com>
2021-09-09 18:06:27 +0400

    removed trace of old site and code in node app

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const radioData = require("./form");
 const cors = require("cors");
 
 mongoose.connect(process.env.DB, {
@@ -20,62 +18,18 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use("/file", express.static("static"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app
-  .route("/form")
-  .get((req, res) => res.render("form", { controls: radioData }))
-  .post((req, res) => {
-    const response = new Response({
-      response: req.body,
-    });
-    response.save();
-    res.redirect("https://abban-fahim.github.io/gp-project/success.html");
+app.route("/form").post((req, res) => {
+  const response = new Response({
+    response: req.body,
   });
-
-// app.route("/video").get((req, res) => res.render("video"));
-
-// app.get("/data", (req, res) => {
-//   res.render("auth", { success: true });
-// });
-// app.post("/auth", (req, res) => {
-//   if (
-//     req.body.user === process.env.USER &&
-//     req.body.password === process.env.PASS
-//   ) {
-//     Response.find((err, docs) => {
-//       res.render("table", { data: docs });
-//     });
-//   } else {
-//     res.render("auth", { success: false });
-//   }
-// });
+  response.save();
+  res.redirect("https://abban-fahim.github.io/gp-project/success.html");
+});
 
 app.get("/getFormJson", (req, res) => {
   Response.find((err, docs) => {
     res.json(docs);
-  });
-});
-
-app.get("/delete/:id", (req, res) => {
-  Response.deleteOne({ _id: req.params.id }, {}, (err) => {
-    err ? console.error(err) : res.redirect("/data");
-  });
-});
-
-app.get("/poem", (req, res) => {
-  res.render("poem");
-});
-
-app.get("/allData", (req, res) => {
-  Response.find({}, (err, found) => {
-    err ? console.error(err) : res.json(found);
   });
 });
 
