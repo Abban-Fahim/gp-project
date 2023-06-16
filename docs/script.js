5d9d6914c7b5186500c9e1fa014e14d7d8d51653
tal <mrwcggr121@gmail.com>
2023-06-16 19:17:49 +0400

    added mail to contact us

--- docs/script.js
+++ docs/script.js
@@ -10,12 +10,11 @@ function throttle(fn, wait) {
 const header = document.querySelector("header");
 const headerTempl = document.createElement("template");
 headerTempl.innerHTML = `<div class="container-fluid">
-  <a href="/" class="navbar-brand text-primary">
+  <a href="./index.html" class="navbar-brand text-primary">
     <img
       src="images/gp-logo.png"
       alt="logo"
       width="50px"
-      height="50px"
       class="navbar-brand"
     />
     S&R Health
@@ -33,17 +32,8 @@ headerTempl.innerHTML = `<div class="container-fluid">
   </button>
   <div class="collapse navbar-collapse" id="navbar">
     <ul class="navbar-nav w-100 p-4 justify-content-end">
-    <li class="nav-item">
-        <a class="nav-link" href="/">Home</a>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="./poem.html">Poem</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./form.html">Form</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./poster.html">Poster</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="./articles.html">Articles</a>
@@ -51,6 +41,9 @@ headerTempl.innerHTML = `<div class="container-fluid">
       <li class="nav-item">
         <a class="nav-link" href="./statistics.html">Statistics</a>
       </li>
+      <li class="nav-item">
+      <a class="nav-link" href="./team.html">Team</a>
+    </li>
     </ul>
   </div>
 </div>`;
@@ -60,7 +53,9 @@ const footerTempl = document.createElement("template");
 footerTempl.innerHTML = `<a href="https://github.com/Abban-Fahim/gp-project"
 ><i class="bi bi-github fs-4 me-3"></i
 ></a>
-<small>© Abban Fahim, Talha Ijaz, Moosa Syed. 2020 - 2021</small>`;
+<small>Sports & Recreational Health | Abban Fahim, Talha Ijaz, Moosa Syed. </ small><a href="mailto:atm.sr.health@gmail.com"
+><i class="bi bi-envelope"></i
+></a>`;
 
 header.appendChild(headerTempl.content);
 footer.appendChild(footerTempl.content);
